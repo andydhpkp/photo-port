a8b73c27c6f7becdbfa7e509cf5bc1a5c9289c22
Andrew Durham <andrewdurham1094@gmail.com>
2021-12-06 17:19:48 -0700

    added Gallery component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import Nav from './components/Nav'
 import About from './components/About';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         {/* components look like any other JSX element, using PascalCase can help you distinguish */}
         <About></About>
       </main>
